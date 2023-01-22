916399effb879cd2ce8aed39016170e0b6f8b309
Luis-Pablo <pabloarias024@gmail.com>
2023-01-22 01:35:54 +0000

    muestra los resultados, integrar intervalos de tiempo y boton con condicional

--- src/app.js
+++ src/app.js
@@ -9,3 +9,40 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };*/
+const numeros = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const pintas = ["♦", "♥", "♠", "♣"];
+let numero = () => {
+  return Math.floor(Math.random() * numeros.length);
+};
+let pinta = () => {
+  return Math.floor(Math.random() * pintas.length);
+};
+//let imprimirPinta = pinta();
+let pinta1 = document.querySelector(".pinta1");
+let pinta2 = document.querySelector(".pinta2");
+let numeroDiv = document.querySelector(".numero1");
+
+let random = () => {
+  pinta1.innerHTML = pinta();
+  pinta2.innerHTML = pinta();
+  numeroDiv.innerHTML = numero();
+};
+setInterval(random(), 2000);
+const boton = document.querySelector(".boton");
+boton.addEventListener("click", () => {
+  random();
+});
